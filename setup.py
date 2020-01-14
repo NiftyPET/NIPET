ecf50047b4c388601aeac8202638e153abbb6a09
Casper da Costa-Luis <casper.dcl@physics.org>
2020-01-14 19:46:39 +0000

    update deps

--- setup.py
+++ setup.py
@@ -258,8 +258,8 @@ setup(
     author_email='p.markiewicz@ucl.ac.uk',
     url='https://github.com/NiftyPET/NiftyPET',
     keywords='PET image reconstruction and analysis',
-    install_requires=['nimpa>=1.2.0', 'pydicom>=1.0.2,<=1.2.2',
-      'nibabel>=2.2.1, <=2.3.1', 'tqdm>=4.27'],
+    install_requires=['nimpa>=2.0.0', 'pydicom>=1.0.2,<=1.2.2',
+      'nibabel>=2.2.1, <=2.3.1', 'tqdm>=4.27', 'numpy>=1.14'],
     python_requires='>=3.4',
     packages=find_packages(exclude=['docs']),
     package_data={
