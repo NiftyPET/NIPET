a224499caf30027199ca6113719b6de7f873c288
Casper da Costa-Luis <casper.dcl@physics.org>
2020-12-17 23:42:52 +0000

    tests: add aligned recon

--- tests/test_amyloid_pvc.py
+++ tests/test_amyloid_pvc.py
@@ -1,9 +1,11 @@
-import sys
 import logging
+import sys
+from collections import Iterable
 from os import path
 from textwrap import dedent
 
 import numpy as np
+import pytest
 
 from niftypet import nimpa
 from niftypet import nipet
@@ -32,13 +34,10 @@ def test_histogramming(folder_in, folder_ref):
 
     # prompt counts: head curve & sinogram
     assert np.sum(hst["phc"]) == np.sum(hst["psino"])
-
     # delayed counts: head curve & sinogram
     assert np.sum(hst["dhc"]) == np.sum(hst["dsino"])
-
     # prompt counts: head curve & reference
     assert np.sum(hst["phc"]) == refpaths["histo"]["p"]
-
     # delayed counts: head curve & reference
     assert np.sum(hst["dhc"]) == refpaths["histo"]["d"]
 
@@ -50,7 +49,6 @@ def test_basic_reconstruction(folder_in, folder_ref, tmp_path):
 
     muhdct = nipet.hdw_mumap(datain, [1, 2, 4], mMRpars, outpath=opth, use_stored=True)
     muodct = nipet.obj_mumap(datain, mMRpars, outpath=opth, store=True)
-
     recon = nipet.mmrchain(
         datain,
         mMRpars,
@@ -66,7 +64,58 @@ def test_basic_reconstruction(folder_in, folder_ref, tmp_path):
     )
 
     testout = {"pet": recon["fpet"], "hmu": muhdct["im"], "omu": muodct["im"]}
-
     for k in testext["basic"]:
         diff = nimpa.imdiff(refpaths["basic"][k], testout[k], verbose=True, plot=False)
-        assert diff["mape"] <= emape_basic
+        assert diff["mape"] <= emape_basic, testext["basic"][k]
+
+
+@pytest.mark.parametrize("reg_tool", ["niftyreg", "spm"])
+def test_aligned_reconstruction(reg_tool, folder_in, folder_ref, tmp_path):
+    datain = nipet.classify_input(folder_in, mMRpars)
+    refpaths, testext = nipet.resources.get_refimg(folder_ref)
+    opth = str(tmp_path / "aligned_reconstruction")
+
+    muhdct = nipet.hdw_mumap(datain, [1, 2, 4], mMRpars, outpath=opth, use_stored=True)
+    muopct = nipet.align_mumap(
+        datain,
+        mMRpars,
+        outpath=opth,
+        t0=0,
+        t1=3600,
+        reg_tool=reg_tool,
+        store=True,
+        itr=2,
+        petopt="ac",
+        fcomment="",
+        musrc="pct",
+    )
+    # matshow(muopct['im'][60,:,:] + muhdct['im'][60,:,:])
+    recon = nipet.mmrchain(
+        datain,
+        mMRpars,
+        frames=["timings", [80, 260], [3000, 3600]],
+        mu_h=muhdct,
+        mu_o=muopct,
+        itr=4,
+        fwhm=0.0,
+        outpath=opth,
+        trim=True,
+        pvcroi=pvcroi,
+        pvcreg_tool=reg_tool,
+        store_img=True,
+        store_img_intrmd=True,
+    )
+
+    testout = {
+        "pet": recon["fpet"],
+        "hmu": muhdct["im"],
+        "omu": muopct["im"],
+        "pos": muopct["fpet"],
+        "trm": recon["trimmed"]["fpet"],
+        "pvc": recon["trimmed"]["fpvc"],
+    }
+    for k in testext["aligned"]:
+        diff = nimpa.imdiff(
+            refpaths["aligned"][reg_tool][k], testout[k], verbose=True, plot=False
+        )
+        assert diff["mape"] <= emape_algnd[k], testext["aligned"][k]
