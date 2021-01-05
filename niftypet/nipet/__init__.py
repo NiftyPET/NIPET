23304d7b16a82436ab8fb26e3b90dadf896144a6
Casper da Costa-Luis <casper.dcl@physics.org>
2021-01-05 00:49:22 +0000

    dist ver

--- niftypet/nipet/__init__.py
+++ niftypet/nipet/__init__.py
@@ -2,6 +2,16 @@
 """initialise the NiftyPET NIPET package"""
 __author__      = ("Pawel J. Markiewicz", "Casper O. da Costa-Luis")
 __copyright__   = "Copyright 2020"
+# version detector. Precedence: installed dist, git, 'UNKNOWN'
+try:
+    from ._dist_ver import __version__
+except ImportError:
+    try:
+        from setuptools_scm import get_version
+
+        __version__ = get_version(root="../..", relative_to=__file__)
+    except (ImportError, LookupError):
+        __version__ = "UNKNOWN"
 
 import logging
 import os
