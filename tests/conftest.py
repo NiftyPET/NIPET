1c33a8fe0c1398213682b1147b6d2e36ccc97615
Casper da Costa-Luis <casper.dcl@physics.org>
2021-01-19 23:38:44 +0000

    tests: minor tidy

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,26 @@
 from os import getenv
 from pathlib import Path
 
-import pytest
+from pytest import fixture, skip
 
 HOME = Path(getenv("DATA_ROOT", "~")).expanduser()
 
 
-@pytest.fixture(scope="session")
+@fixture(scope="session")
 def folder_in():
     Ab_PET_mMR_test = HOME / "Ab_PET_mMR_test"
     if not Ab_PET_mMR_test.is_dir():
-        pytest.skip(f"""Cannot find Ab_PET_mMR_test in ${{DATA_ROOT:-~}} ({HOME}).
+        skip(f"""Cannot find Ab_PET_mMR_test in ${{DATA_ROOT:-~}} ({HOME}).
 Try running `python -m tests` to download it.
 """)
     return Ab_PET_mMR_test
 
 
-@pytest.fixture(scope="session")
+@fixture(scope="session")
 def folder_ref(folder_in):
     Ab_PET_mMR_ref = folder_in / "testing_reference" / "Ab_PET_mMR_ref"
     if not Ab_PET_mMR_ref.is_dir():
-        pytest.skip(f"""Cannot find Ab_PET_mMR_ref in
+        skip(f"""Cannot find Ab_PET_mMR_ref in
 ${{DATA_ROOT:-~}}/testing_reference ({HOME}/testing_reference).
 Try running `python -m tests` to download it.
 """)
